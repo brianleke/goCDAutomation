33b667aacfee928cb9df15723df735b0ec32ebff
Brain <brianleke@gmail.com>
2015-08-31 19:28:12 +1000

    [Brain] Adding angularjs skeleton

--- flaskAutomation/files/TestApp.py
+++ flaskAutomation/files/TestApp.py
@@ -6,7 +6,7 @@ class TestApp(unittest.TestCase):
 	def test_index_method(self):
 		response = app.test_client().get('/')
 		self.assertEqual(response.status_code, 200)
-		self.assertEqual(response.data, '<html ng-app="myApp">\n\t<head>\n\t\t<script src="/static/angular.min.js"></script>\n\t</head>\n\t<body>\n\t\t<h1>Test!</h1>\n\t</body>\n</html>')
+		self.assertEqual(response.data, '<html ng-app="myApp">\n\t<head>\n\t\t<script src="/static/angular.min.js"></script>\n\t\t<script src="/static/myApp.js"></script>\n\t</head>\n\t<body>\n\t\t<h2>Test!</h2>\n\t\t<div ng-view></div>\n\t</body>\n</html>')
 
 	def test_data_returns_jsonified_data(self):
 		response = app.test_client().get('/data')
